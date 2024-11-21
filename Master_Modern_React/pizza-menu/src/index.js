9a16934435c936ec93ca3557ca1371fb22e0f2ae
virag-ky <vkormoczy@gmail.com>
2024-11-21 14:52:33 +0200

    Add JS expressions in components

--- Master_Modern_React/pizza-menu/src/index.js
+++ Master_Modern_React/pizza-menu/src/index.js
@@ -82,6 +82,17 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  // if (hour >= openHour && hour <= closeHour) {
+  //   alert("We're currently open!");
+  // } else {
+  //   alert("Sorry, we're closed!");
+  // }
+
   return (
     <footer>{new Date().toLocaleTimeString()} We're currently open!</footer>
   );
