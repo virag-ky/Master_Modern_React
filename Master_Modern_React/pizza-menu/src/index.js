e544ab5f737e9ec49fb6b6fc3b283ab34d4aaf35
Virag Kormoczy <vkormoczy@gmail.com>
2024-12-23 13:39:59 +0200

    Conditional rendering with ternary operator

--- Master_Modern_React/pizza-menu/src/index.js
+++ Master_Modern_React/pizza-menu/src/index.js
@@ -72,12 +72,14 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      {numPizzas > 0 && (
+      {numPizzas > 0 ? (
         <ul className="pizzas">
           {pizzaData.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later!</p>
       )}
     </main>
   );
@@ -105,11 +107,15 @@ function Footer() {
   return (
     <footer className="footer">
       {new Date().toLocaleTimeString()}
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
   );
