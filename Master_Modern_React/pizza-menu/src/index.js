5d8e80fef2f7b1d489c80d7699055493af6cd35c
Virag Kormoczy <vkormoczy@gmail.com>
2024-12-27 12:22:25 +0200

    Extracting JSX into a new component

--- Master_Modern_React/pizza-menu/src/index.js
+++ Master_Modern_React/pizza-menu/src/index.js
@@ -118,10 +118,7 @@ function Footer() {
     <footer className="footer">
       {new Date().toLocaleTimeString()}
       {isOpen ? (
-        <div className="order">
-          <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-          <button className="btn">Order</button>
-        </div>
+        <Order closeHour={closeHour} />
       ) : (
         <p>
           We're happy to welcome you between {openHour}:00 and {closeHour}:00.
@@ -130,6 +127,17 @@ function Footer() {
     </footer>
   );
   // return React.createElement("footer", null, "We're currently open!");
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open until {props.closeHour}:00. Come visit us or order online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
