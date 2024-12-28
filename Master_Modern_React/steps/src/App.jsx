932469d334865116ff599fd2186a029d30e54111
virag-ky <vkormoczy@gmail.com>
2024-12-28 15:25:59 +0200

    Create a state variable with useState

--- Master_Modern_React/steps/src/App.jsx
+++ Master_Modern_React/steps/src/App.jsx
@@ -1,4 +1,4 @@
-//import { useState } from "react";
+import { useState } from "react";
 
 const messages = [
   "Learn React ⚛️",
@@ -7,18 +7,26 @@ const messages = [
 ];
 
 function App() {
-  const step = 1;
+  const [step, setStep] = useState(1);
 
-  function handlePrevious() {}
+  function handlePrevious() {
+    if (step > 1) {
+      setStep(step - 1);
+    }
+  }
 
-  function handleNext() {}
+  function handleNext() {
+    if (step < 3) {
+      setStep(step + 1);
+    }
+  }
 
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
       <p className="message">
         Step {step}: {messages[step - 1]}
