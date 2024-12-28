39502a4c101ecff8caa557d1e088ad13bd862507
virag-ky <vkormoczy@gmail.com>
2024-12-28 13:51:30 +0200

    Using event handlers

--- Master_Modern_React/steps/src/App.jsx
+++ Master_Modern_React/steps/src/App.jsx
@@ -9,6 +9,10 @@ const messages = [
 function App() {
   const step = 1;
 
+  function handlePrevious() {}
+
+  function handleNext() {}
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -20,10 +24,16 @@ function App() {
         Step {step}: {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
