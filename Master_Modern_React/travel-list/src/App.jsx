dfb4f8a4ea5c40d3cb5153c5a32cd555533c5b90
virag-ky <vkormoczy@gmail.com>
2024-12-29 15:37:38 +0200

    Use controlled elements

--- Master_Modern_React/travel-list/src/App.jsx
+++ Master_Modern_React/travel-list/src/App.jsx
@@ -23,21 +23,41 @@ function Logo() {
 }
 
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    setQuantity(1);
+    setDescription("");
   }
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
-      <select name="number-of-items" id="number-of-items">
+      <select
+        name="quantity"
+        id="quantity"
+        value={quantity}
+        onChange={(e) => setQuantity(+e.target.value)}
+      >
         {Array.from({ length: 20 }, (_, index) => index + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
   );
