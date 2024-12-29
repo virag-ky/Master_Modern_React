90454c4995eecb2cb2ce2900c5a288dec5ddc53a
virag-ky <vkormoczy@gmail.com>
2024-12-29 14:42:34 +0200

    Render the items list

--- Master_Modern_React/travel-list/src/App.jsx
+++ Master_Modern_React/travel-list/src/App.jsx
@@ -1,4 +1,11 @@
+/* eslint-disable react/prop-types */
 import { useState } from "react";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
+];
 
 function App() {
   return (
@@ -24,12 +31,31 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item key={item.description} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>🧳 You have X items on your list and you already packed X (X%)</em>
     </footer>
   );
