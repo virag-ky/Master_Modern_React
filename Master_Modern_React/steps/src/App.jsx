31fc6db95187fd2d096fe43f6ac5a11217dfec14
virag-ky <vkormoczy@gmail.com>
2025-01-31 17:33:41 +0200

    Make a reusable StepMessage component

--- Master_Modern_React/steps/src/App.jsx
+++ Master_Modern_React/steps/src/App.jsx
@@ -42,9 +42,9 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+
           <div className="buttons">
             <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
               <span>👈🏻</span> Previous
@@ -56,6 +56,15 @@ function Steps() {
         </div>
       )}
     </>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
   );
 }
 
