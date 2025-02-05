b4cb9943bbe20200ecebbf810c836833ad1e9feb
virag-ky <vkormoczy@gmail.com>
2025-02-05 16:25:28 +0200

    Add event handling to the Button component

--- Master_Modern_React/eat-n-split/src/App.jsx
+++ Master_Modern_React/eat-n-split/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 /* eslint-disable react/prop-types */
 const initialFriends = [
   {
@@ -20,13 +22,29 @@ const initialFriends = [
   },
 ];
 
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
 function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !show);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriend />
-        <Button>Add friend</Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add friend"}
+        </Button>
       </div>
       <FormSplitBill />
     </div>
@@ -78,10 +96,6 @@ function FormAddFriend() {
       <Button>Add</Button>
     </form>
   );
-}
-
-function Button({ children }) {
-  return <button className="button">{children}</button>;
 }
 
 function FormSplitBill() {
